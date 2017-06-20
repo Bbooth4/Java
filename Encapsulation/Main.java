4c245f5221d906825fd7c7266bb9bd66a072d627
Brandon <booth.brandon4@gmail.com>
2017-06-20 16:24:06 -0400

    a printer class created

--- Encapsulation/Main.java
+++ Encapsulation/Main.java
@@ -17,5 +17,15 @@ public class Main {
     EnhancedPlayer player = new EnhancedPlayer("Brandon", 50, "Fists");
     // since I am calling the getHealth() method instead of directly grabbing it like from above, if I change the name of the variables here (health to hp), then it will still work, I don't need to know the variable names on this side 
     System.out.println("Initial health is " + player.getHealth());
+
+    Printer printerTrue = new Printer(50, 0, true);
+    Printer printerFalse = new Printer(50, 0, false);
+
+    printerTrue.fillToner(7);
+    printerTrue.fillToner(5);
+    printerTrue.printPages(15);
+
+    printerFalse.fillToner(12);
+    printerFalse.printPages(20);
   }
 }
---
+++ Encapsulation/Printer.java
@@ -0,0 +1,34 @@
+public class Printer {
+  private int toner;
+  private int pages;
+  private boolean duplex;
+
+  public Printer(int toner, int pages, boolean duplex) {
+    this.toner = toner;
+    this.pages = pages;
+    this.duplex = duplex;
+  }
+
+  public void fillToner(int t) {
+    this.toner += t; 
+    if (this.toner > 100) {
+      this.toner = 100;
+      System.out.println("Current toner " + this.toner);
+    } else if (this.toner < 0) {
+      this.toner = 0;
+      System.out.println("Current toner " + this.toner);
+    }
+  }
+
+  public void printPages(int p) {
+    if (this.duplex = true) {
+      this.pages += p/2;
+      this.toner -= p/2;
+      System.out.println("Current toner " + this.toner + " and current pages " + this.pages);
+    } else {
+      this.pages += p;
+      this.toner -= p;
+      System.out.println("Current toner " + this.toner + " and current pages " + this.pages);
+    }
+  }
+}
